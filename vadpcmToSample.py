7287c4fe48420be46f9683cf94ad16a3da101907
Captain Kitty Cat <68467449+Captainkittyca2@users.noreply.github.com>
2023-11-21 10:04:11 +0300

    Add requirement when first using the tool

--- vadpcmToSample.py
+++ vadpcmToSample.py
@@ -6,8 +6,8 @@ import os
 class UI(QMainWindow):
     openFile = False
     fileOpen = False
-    audioSampledefault = b'\x00\x00\x00\x00\x50\x4D\x53\x4F\x02\x00\x00\x00\xEF\xBE\xAD\xDE\xEF\xBE\xAD\xDE\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-    audioSampledefault2 = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x04\x00\x00\x00\x40\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x79\x07\xFC\x06\x87\x06\x19\x06\xB3\x05\x53\x05\xFA\x04\xA7\x04\x80\xF8\xB4\xF1\xC8\xEB\xDB\xE6\x07\xE3\x58\xE0\xD1\xDE\x6B\xDE\x40\x0F\x92\x15\xD2\x1A\xE8\x1E\xC5\x21\x66\x23\xD2\x23\x18\x23\x80\xF9\x52\xF4\x4C\xF0\x45\xED\x1B\xEB\xAC\xE9\xDB\xE8\x8E\xE8\x60\x0E\x54\x13\x0D\x17\xB8\x19\x7B\x1B\x7C\x1C\xDB\x1C\xB5\x1C'
+    audioSampledefault = b''
+    audioSampledefault2 = b''
     dataa = None
     fname = tuple
     def __init__(self):
@@ -16,17 +16,51 @@ class UI(QMainWindow):
         self.setFixedSize(340,319)
 
         self.spinBox = self.findChild(QSpinBox, 'spinBox')
+        self.spinBox2 = self.findChild(QSpinBox, 'spinBox_2')
+        self.spinBox3 = self.findChild(QSpinBox, 'spinBox_3')
         self.label_3 = self.findChild(QLabel, 'label_3')
+        self.label_4 = self.findChild(QLabel, 'label_4')
         self.label_5 = self.findChild(QLabel, 'label_5')
+        self.label_6 = self.findChild(QLabel, 'label_6')
         self.pushbutton = self.findChild(QPushButton, 'pushButton')
         self.pushbutton2 = self.findChild(QPushButton, 'pushButton_2')
 
         self.spinBox.hide()
+        self.spinBox2.hide()
+        self.spinBox3.hide()
         self.label_3.hide()
+        self.label_4.hide()
+        self.label_6.hide()
         self.pushbutton2.hide()
         self.pushbutton.pressed.connect(self.Open)
         self.pushbutton2.pressed.connect(self.Convert)
         self.show()
+        try:
+            fyuyuyle = open("config.bruh", 'rb')
+            self.audioSampledefault = fyuyuyle.read(72)
+            self.audioSampledefault2 = fyuyuyle.read(150)
+        except FileNotFoundError:
+            self.firstTime()
+
+    def firstTime(self):
+        fnume = QFileDialog.getOpenFileName(self, 'Open a Soh Audio Sample', '', 'SoH Audio Sample (*)')
+        if fnume[0]:
+            fuyle = open(fnume[0], 'rb+')
+            fuyle.seek(4)
+            if b'\x50\x4D\x53\x4F' in fuyle.read(4):
+                openFile = True
+                fuyle.seek(0)
+                fayayle = open('config.bruh', 'wb+')
+                fayayle.write(fuyle.read(72))
+                boi = os.path.getsize(fnume[0]) - 215
+                fuyle.seek(71+boi)
+                fayayle.write(fuyle.read(156))
+                fayayle.close()
+                fuyle.close()
+                sys.exit()
+            else: sys.exit()
+        else: sys.exit()
+
 
     def Open(self):
         fname = QFileDialog.getOpenFileName(self, 'Open vadpcm.bin File', '', 'vadpcm.bin (*.vadpcm.bin);; All Files (*)')
@@ -34,7 +68,11 @@ class UI(QMainWindow):
             if self.fileOpen == False:
                 self.fileOpen = True
                 self.spinBox.show()
+                self.spinBox2.show()
+                self.spinBox3.show()
                 self.label_3.show()
+                self.label_4.show()
+                self.label_6.show()
                 self.pushbutton2.show()
             self.fname = fname
             self.label_5.setText(fname[0])
@@ -51,16 +89,30 @@ class UI(QMainWindow):
                 self.label_5.setText("Can't Find config.toml file (z64audio generates a config.toml file)")
                 self.fileOpen = False
                 self.spinBox.hide()
+                self.spinBox2.hide()
+                self.spinBox3.hide()
                 self.label_3.hide()
+                self.label_4.hide()
+                self.label_6.hide()
                 self.pushbutton2.hide()
                 return
+            x = ""
             y = ""
+            z = ""
+            while ("loop_start" not in x):
+                x = foile.readline()
             while ("loop_end" not in y):
                 y = foile.readline()
+            while ("loop_count" not in z):
+                z = foile.readline()
+            loop_start = [int(word) for word in x.split() if word.isdigit()][0]
             loop_end = [int(word) for word in y.split() if word.isdigit()][0]
+            loop_count = [int(word) for word in z.split() if word.isdigit()][0]
             self.spinBox.setValue(loop_end)
+            self.spinBox2.setValue(loop_count)
+            self.spinBox3.setValue(loop_start)
     def Convert(self):
-        fneme = QFileDialog.getSaveFileName(self, 'Save as SoH audio Sample', '', '')
+        fneme = QFileDialog.getSaveFileName(self, 'Save as SoH audio Sample', '', 'SoH audio Sample (*)')
         if fneme[0]:
             fyle = open(fneme[0], 'wb+')
             fyle.write(self.audioSampledefault)
@@ -74,7 +126,14 @@ class UI(QMainWindow):
             fyle.write(swep)
             fyle.seek(72)
             fyle.write(self.dataa)
-            fyle.write(b'\x00\x00\x00\x00')
+            fyle.write(self.spinBox3.value().to_bytes(2))
+            fyle.seek(72+len(self.dataa))
+            swep = fyle.read(1)
+            swap = fyle.read(1)
+            fyle.seek(72+len(self.dataa))
+            fyle.write(swap)
+            fyle.write(swep)
+            fyle.write(b'\x00\x00')
             fyle.write(self.spinBox.value().to_bytes(2))
             fyle.seek(76+len(self.dataa))
             swep = fyle.read(1)
@@ -82,6 +141,15 @@ class UI(QMainWindow):
             fyle.seek(76+len(self.dataa))
             fyle.write(swap)
             fyle.write(swep)
+            fyle.write(b'\x00\x00')
+            fyle.write(self.spinBox2.value().to_bytes(2))
+            fyle.seek(80+len(self.dataa))
+            swep = fyle.read(1)
+            swap = fyle.read(1)
+            fyle.seek(80+len(self.dataa))
+            fyle.write(swap)
+            fyle.write(swep)
+            fyle.write(b'\x00\x00')
             fyle.write(self.audioSampledefault2)
             fyle.close()
 
@@ -90,4 +158,3 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     UIWindow = UI()
     app.exec_()
-
